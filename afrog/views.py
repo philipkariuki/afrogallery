a7fc51bf561f93446d1d1b3a19e905c99b20241d
philipkariuki <kariukiphil@gmail.com>
2019-08-15 16:15:44 +0300

    in views.py imports most of the required django modules creates forms.py

--- afrog/views.py
+++ afrog/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *  # Project, UserProfile, Comments //
+from .forms import *   # CommentForm, NewProjectForm, UserForm,  ProfileForm //
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 # Create your views here.
 def index(request):
