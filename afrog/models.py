934085539f75479cba4f5cdc58e04118f9b2754c
philipkariuki <kariukiphil@gmail.com>
2019-08-16 10:22:50 +0300

    imports most of the required modules in models.py creates first model UserProfile

--- afrog/models.py
+++ afrog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length =260)    
+    pub_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(default=0, max_length =14)
+    pic = models.ImageField(upload_to = 'profilepics2/', blank=True )
+    projects = models.CharField(max_length =420)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
