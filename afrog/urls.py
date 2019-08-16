771a067cd8f007ebe706c45be2a1b7cf9e4fb219
philipkariuki <kariukiphil@gmail.com>
2019-08-16 11:02:03 +0300

    adds the necessary configurations in app's urls.py to display userprofile.pic and confirms it works

--- afrog/urls.py
+++ afrog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^profile/',views.profile,name ='profile'),
     url(r'^profile_update/',views.update_profile,name='profile_update'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
